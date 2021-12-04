ce5ffd826abd38d6e46f0e59e3ce0978b0263c4d
Yoona8 <elena243v@gmail.com>
2021-12-04 17:41:27 +0300

    Add quadratic equation

---
+++ solutions/chapter-2/quadratic-equation.js
@@ -0,0 +1,11 @@
+// write a function y = ax^2 + bx + c
+
+function solveQuadraticEquation(a, x, b, c) {
+	return a * x * x + b * x + c;
+}
+
+console.log(solveQuadraticEquation(-8, -5, -2, 7)); // => -183
+console.log(solveQuadraticEquation(8, 2, 9, -10)); // => 40
+console.log(solveQuadraticEquation(0, 0, 0, 0)); // => 0
+console.log(solveQuadraticEquation(5, -2, 5, 128)); // => 138
+console.log(solveQuadraticEquation(10, 49, 6, 19)); // => 24323
